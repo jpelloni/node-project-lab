857898d4d7a4d42202fb2c97fc6311fd0308afb5
Jason Pelloni <jpelloni@gmail.com>
2026-07-22 16:10:58 +0000

    feat: add semester 1 lesson 7 exercises

---
+++ semesters/semester-1/lesson-7/src/exercise-1.js
@@ -0,0 +1,8 @@
+console.log(process.pid);
+
+console.log(process.ppid);
+
+// What changes between runs?
+// the process ID (pid) changed
+// What stays the same?
+// the parent process ID (ppid) stays the same
---
+++ semesters/semester-1/lesson-7/src/process-info.js
@@ -0,0 +1,8 @@
+console.log("Process ID:", process.pid);
+console.log("Parent Process ID:", process.ppid);
+console.log("Node version:", process.version);
+console.log("Platform:", process.platform);
+console.log("Architecture:", process.arch);
+console.log("Current working directory:", process.cwd());
+console.log("Executable path:", process.execPath);
+console.log("Memory usage:", process.memoryUsage());
